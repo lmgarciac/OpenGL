f74d40608e83f15a3fa13d4f50c65c107c3a03e0
lmgarciac <30421890+lmgarciac@users.noreply.github.com>
2025-04-09 17:59:58 -0300

    More Cleanup
    More Cleanup

--- OpenGLProject/OpenGLProject.cpp
+++ OpenGLProject/OpenGLProject.cpp
@@ -1,38 +1,25 @@
-#include <iostream>
+//Standard
 #include <stdio.h>
-#include "string.h"
-#include "stdlib.h"
-#include <math.h>
-#include "vector"
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <vector>
 
+//GLM
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+
+//Engine
 #include "Mesh.h"
 #include "Shader.h"
 #include "Canvas.h"
 
-//Window dimensions
-const GLint WIDTH = 800, HEIGHT = 600;
-const float toRadians = 3.14159265f / 180.0f;
-
-Canvas mainWindow(1024, 768);
+Canvas mainWindow(800, 600);
 std::vector<Mesh*> meshList;
 std::vector<Shader*> shaderList;
-
-bool direction = true;
-float triOffset = 0.0f;
-float triMaxoffset = 0.7f;
-float triIncrement = 0.005f;
-
-float curAngle = 0.0f;
-
-bool sizeDirection = true;
-float curSize = 0.5f;
-float maxSize = 0.8f;
-float minSize = 0.1f;
 
 //Vertex Shader
 static const char* vShader = "Shaders/shader.vert";
@@ -92,39 +79,6 @@ int main()
 		//Get + Handle user input events
 		glfwPollEvents();
 
-		if (direction)
-		{
-			triOffset += triIncrement;
-		}
-		else
-		{
-			triOffset -= triIncrement;
-		}
-
-		if (abs(triOffset) >= triMaxoffset)
-			direction = !direction;
-
-		curAngle += 0.5f;
-
-		if (curAngle >= 360)
-		{
-			curAngle -= 360;
-		}
-
-		if (sizeDirection)
-		{
-			curSize += 0.001f;
-		}
-		else
-		{
-			curSize -= 0.001f;
-		}
-
-		if (curSize >= maxSize || curSize <= minSize)
-		{
-			sizeDirection = !sizeDirection;
-		}
-
 		//Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -133,20 +87,16 @@ int main()
 		uniformModel = shaderList[0]->GetModelLocation();
 		uniformProjection = shaderList[0]->GetProjectionLocation();
 
-		
-
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glm::mat4 model(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, triOffset, -2.5f));
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
 		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		meshList[0]->RenderMesh();
 
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(-triOffset, -1.0f, -2.5f));
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(0.0f, -1.0f, -2.5f));
 		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		meshList[1]->RenderMesh();
@@ -155,7 +105,6 @@ int main()
 		glUseProgram(0);
 
 		mainWindow.SwapBuffers();
-
 	}
 
 	printf("Program Finished \n");
